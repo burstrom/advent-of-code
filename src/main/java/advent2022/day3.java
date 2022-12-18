b52398d3be760370c5a910f0e343fb1fd3113180
burstrom <burstrom.johan@gmail.com>
2022-12-18 09:07:00 +0100

    setup for day 3 done, might continue later...

--- src/main/java/advent2022/day3.java
+++ src/main/java/advent2022/day3.java
@@ -5,83 +5,35 @@ import java.util.ArrayList;
 public class day3 {
 
 
+    /**
+     * Each elf loads its rucksacks with supplies.
+     * Each rucksack has two compartments
+     * Every item in the rucksack is defined as a char case sensitive.
+     * First half of the line is packed in the first rucksack, the other half is in the second rucksack
+     * One ItemType per Ruckpack, thus if you find the same characters in both ruckpacks it is wrongly packed.
+     * Priority: lowercase items a-z has priorities 01 - 26
+     * Priority: UPPERCASE items A-Z has priorities 27 - 52
+     */
     public static void main(String[] args){
         ArrayList<String> textInput = HelperClass.readFileLines("input3a.txt");
 
         int result = 0;
         for (String s : textInput) {
-            result += calcWin(s);
+            result += calcPart1(s);
         }
 
         System.out.println("Result : " + result);
     }
-    private static int calcWin(String line){
-        String[] s = line.split(" ");
-        return playScore2(s[0], s[1]);
+
+    private static int calcPart1(String s) {
+
+        int middleIndex = s.length() / 2;
+        String first = s.substring(0, middleIndex);
+        String second = s.substring(middleIndex);
+
+        System.out.println(first +", " + second);
+        return 0;
     }
 
-    // Part 1
-    static int playScore(String a, String b) {
-        int result = switch (a){
-                    case "A" -> b.equals("X") ? 3 : b.equals("Y") ? 6 : 0;
-                    case "B" -> b.equals("Y") ? 3 : b.equals("Z") ? 6 : 0;
-                    default -> b.equals("Z") ? 3 : b.equals("Y") ? 0 : 6;
-            };
-        int i = calcRpsValue(b);
-
-        int resultingScore = result + i;
-        System.out.println(textToScore(a) +" " + textToScore(b) + " -> " + result +", "+ i +" = " + resultingScore);
-        return resultingScore;
-    }
-
-    // Part 2
-    private static int playScore2(String a, String b) {
-        int result = switch (b){
-            case "X" -> 0;
-            case "Y" -> 3;
-            default -> 6;
-        };
-        int i = part2(a,b);
-
-        int resultingScore = result + i;
-        System.out.println(textToScore2(b) +" " + textToScore2(a) + " -> " + result +", "+ i +" = " + resultingScore);
-        return resultingScore;
-    }
-
-    private static int calcRpsValue(String b) {
-        return switch(b){
-            case "A","X" -> 1;
-            case "B","Y" -> 2;
-            default -> 3;
-        };
-    }
-
-    private static String textToScore(String a){
-        return switch(a){
-            case "A","X" -> "Stone (1)";
-            case "B","Y" -> "Paper (2)";
-            default -> "Scissor (3)";
-        };
-    }
-
-    private static String textToScore2(String a){
-        return switch(a){
-            case "A" -> "Stone (1)";
-            case "B" -> "Paper (2)";
-            case "C" -> "Scissor (3)";
-            case "X" -> "Loose (0)";
-            case "Y" -> "Draw (3)";
-            case "Z" -> "Win (6)";
-            default -> "ERROR";
-        };
-    }
-
-    private static int part2(String a, String b){
-        return switch(b){
-            case "X" -> a.equals("A") ? 3 :a.equals("B") ? 1 : 2;
-            case "Y" -> a.equals("A") ? 1 :a.equals("B") ? 2 : 3;
-            default -> a.equals("A") ? 2 :a.equals("B") ? 3 : 1;
-        };
-    }
 
 }
